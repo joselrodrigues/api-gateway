f8242b83404c41f6d9b3c8f9cb907f3b0529529b
joselrodrigues <ing.joserodrigues@gmail.com>
2023-05-19 16:06:43 -0400

    feature: send .env config in the fiber ctx local

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"apigateway/config"
 	"apigateway/routes"
 	"log"
 
@@ -9,6 +10,18 @@ import (
 
 func main() {
 	app := fiber.New()
+
+	cfg, err := config.LoadConfig(".")
+
+	if err != nil {
+		log.Fatalf("failed to load configuration: %v", err)
+	}
+
+	app.Use(func(c *fiber.Ctx) error {
+		c.Locals("cfg", cfg)
+		return c.Next()
+	})
+
 	routes.Setup(app)
 	log.Fatal(app.Listen(":3000"))
 }
